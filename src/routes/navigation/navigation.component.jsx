d54e392bd501391a08a64aa98a9c75653241d4e0
franciscobtd <franciscobernabe@drata.com>
2022-04-22 22:34:20 -0500

    feat: add signin page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,6 +14,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+          <Link className="nav-link" to="/shop">
+            Sign In
+          </Link>
         </div>
       </div>
       <Outlet />
